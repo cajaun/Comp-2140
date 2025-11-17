f3ff769a64f253ce43187861e9e703155e4f9544
cajaun <cajaun.campbell@gmail.com>
2025-11-17 14:16:51 -0500

    Pushing java file

---
+++ Mileage.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Mileage {
+    public static void main(String[] args) {
+        int miles;
+        double gallons, mpg;
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Enter miles and gallons: ");
+        miles = s.nextInt();
+        gallons = s.nextDouble();
+
+        mpg = miles / gallons;
+
+      
+        System.out.println("Miles Per Gallon: " + mpg);
+    }
+}
